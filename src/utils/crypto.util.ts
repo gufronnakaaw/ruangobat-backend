54f76799e1925833fa4defa10cb7c7126dc08280
gufronnakaaw <gufronnakaaw@gmail.com>
2024-09-28 15:47:11 +0700

    update: function name

--- src/utils/crypto.util.ts
+++ src/utils/crypto.util.ts
@@ -1,9 +1,9 @@
 import { AES, enc } from 'crypto-js';
 
-export function encryptPassword(password: string, key: string): string {
-  return AES.encrypt(password, key).toString();
+export function encryptString(strings: string, key: string): string {
+  return AES.encrypt(strings, key).toString();
 }
 
-export function decryptPassword(encrypted: string, key: string): string {
+export function decryptString(encrypted: string, key: string): string {
   return AES.decrypt(encrypted, key).toString(enc.Utf8);
 }
