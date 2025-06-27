fde05860e1fc44dd20c68826f94e4bc8488f51ce
gufronnakaaw <gufronnakaaw@gmail.com>
2025-06-27 12:49:59 +0700

    feat(quiz): add crud quiz

---
+++ src/quizzes/quizzes.dto.ts
@@ -0,0 +1,85 @@
+import { z } from 'zod';
+
+export type QuizzesQuery = {
+  q?: string;
+  page: string;
+};
+
+export const createQuizSchema = z.object({
+  category_id: z.string().uuid(),
+  sub_category_id: z.string().uuid(),
+  title: z
+    .string()
+    .trim()
+    .transform((val) => val.replace(/\s+/g, ' ')),
+  description: z
+    .string()
+    .trim()
+    .transform((val) => val.replace(/\s+/g, ' '))
+    .optional(),
+  type: z.enum(['videocourse', 'apotekerclass', 'videoukmppai']),
+  variant: z.enum(['quiz', 'tryout']),
+  questions: z
+    .array(
+      z.object({
+        number: z.number().positive().optional(),
+        text: z.string(),
+        explanation: z.string().optional(),
+        url: z.string().optional(),
+        type: z.enum(['text', 'video', 'image']),
+        options: z
+          .array(
+            z.object({
+              text: z.string(),
+              is_correct: z.boolean(),
+            }),
+          )
+          .min(5, { message: 'Harus diisi minimal 5 opsi' }),
+      }),
+    )
+    .min(1, { message: 'Harus diisi minimal 1 soal' }),
+  by: z.string(),
+});
+
+export type CreateQuizDto = z.infer<typeof createQuizSchema>;
+
+export const updateQuizSchema = z.object({
+  ass_id: z.string(),
+  title: z
+    .string()
+    .trim()
+    .transform((val) => val.replace(/\s+/g, ' '))
+    .optional(),
+  description: z
+    .string()
+    .trim()
+    .transform((val) => val.replace(/\s+/g, ' '))
+    .optional(),
+  questions: z
+    .array(
+      z.object({
+        assq_id: z.string().optional(),
+        number: z.number().positive().optional(),
+        text: z.string().optional(),
+        explanation: z.string().optional(),
+        url: z.string().optional(),
+        type: z.enum(['text', 'video', 'image']).optional(),
+        options: z
+          .array(
+            z.object({
+              asso_id: z.string().optional(),
+              text: z.string().optional(),
+              is_correct: z.boolean().optional(),
+            }),
+          )
+          .min(5, { message: 'Harus diisi minimal 5 opsi' })
+          .optional(),
+      }),
+    )
+    .min(1, { message: 'Harus diisi minimal 1 soal' })
+    .optional(),
+  by: z.string(),
+  update_type: z.enum(['update_quiz', 'update_question', 'add_question']),
+});
+
+export type UpdateQuizDto = z.infer<typeof updateQuizSchema>;
---
+++ src/quizzes/quizzes.service.ts
@@ -0,0 +1,310 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { random } from 'lodash';
+import ShortUniqueId from 'short-unique-id';
+import { PrismaService } from '../utils/services/prisma.service';
+import { CreateQuizDto, QuizzesQuery, UpdateQuizDto } from './quizzes.dto';
+
+@Injectable()
+export class QuizzesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getQuizzes(
+    cat_or_sub: string,
+    type: 'apotekerclass' | 'videocourse' | 'videoukmppai',
+    variant: 'quiz' | 'tryout',
+    role: string,
+  ) {
+    if (
+      !['apotekerclass', 'videocourse', 'videoukmppai'].includes(type) ||
+      !['quiz', 'tryout'].includes(variant)
+    ) {
+      return [];
+    }
+
+    let model: 'category' | 'sub_category' | '' = '';
+
+    if (type === 'apotekerclass') {
+      model += 'category';
+    } else {
+      model += 'sub_category';
+    }
+
+    return this.prisma.assessment
+      .findMany({
+        where: {
+          ass_type: type,
+          variant,
+          [model]: {
+            OR: [{ [model + '_id']: cat_or_sub }, { slug: cat_or_sub }],
+          },
+          ...(role === 'admin' ? { is_active: true } : {}),
+        },
+        select: {
+          ass_id: true,
+          description: true,
+          title: true,
+          variant: true,
+          _count: {
+            select: {
+              question: true,
+            },
+          },
+        },
+      })
+      .then((assessment) => {
+        if (!assessment.length) return [];
+
+        return assessment.map((item) => {
+          const { _count, ...rest } = item;
+
+          return {
+            ...rest,
+            total_questions: _count.question,
+          };
+        });
+      });
+  }
+
+  async getQuiz(ass_id: string, query: QuizzesQuery) {
+    const default_page = 1;
+    const take = 20;
+
+    const page = parseInt(query.page) ? parseInt(query.page) : default_page;
+
+    const skip = (page - 1) * take;
+
+    const [total_questions, quiz] = await this.prisma.$transaction([
+      this.prisma.assessmentQuestion.count({ where: { ass_id } }),
+      this.prisma.assessment.findUnique({
+        where: { ass_id },
+        select: {
+          ass_id: true,
+          title: true,
+          description: true,
+          question: {
+            select: {
+              assq_id: true,
+              number: true,
+              text: true,
+              explanation: true,
+              type: true,
+              option: {
+                select: {
+                  asso_id: true,
+                  text: true,
+                  is_correct: true,
+                },
+              },
+            },
+            orderBy: { number: 'asc' },
+            take,
+            skip,
+          },
+        },
+      }),
+    ]);
+
+    return {
+      ...quiz,
+      page: parseInt(query.page),
+      total_questions,
+      total_pages: Math.ceil(total_questions / take),
+    };
+  }
+
+  async createQuiz(body: CreateQuizDto) {
+    const ass_id = `ROASS${random(10000000, 99999999)}`;
+    const uid = new ShortUniqueId({ length: 12 });
+
+    const promises = [];
+
+    for (const [index, question] of body.questions.entries()) {
+      promises.push(
+        this.prisma.assessmentQuestion.create({
+          data: {
+            ass_id,
+            assq_id: `ROQ${uid.rnd().toUpperCase()}`,
+            text: question.text,
+            explanation: question.explanation,
+            url: question.url,
+            type: question.type,
+            number: question.number ? question.number : index + 1,
+            created_by: body.by,
+            updated_by: body.by,
+            option: {
+              createMany: {
+                data: question.options.map((option) => ({
+                  asso_id: `ROO${uid.rnd().toUpperCase()}`,
+                  text: option.text,
+                  is_correct: option.is_correct,
+                  created_by: body.by,
+                  updated_by: body.by,
+                })),
+              },
+            },
+          },
+        }),
+      );
+    }
+
+    await this.prisma.assessment.create({
+      data: {
+        ass_id,
+        category_id: body.category_id,
+        sub_category_id: body.sub_category_id,
+        title: body.title,
+        description: body.description,
+        ass_type: body.type,
+        created_by: body.by,
+        updated_by: body.by,
+        variant: body.variant,
+      },
+    });
+
+    await Promise.all(promises);
+
+    return {
+      ass_id,
+    };
+  }
+
+  async updateQuiz(body: UpdateQuizDto) {
+    const quiz = await this.prisma.assessment.count({
+      where: { ass_id: body.ass_id },
+    });
+
+    if (!quiz) {
+      throw new NotFoundException('Quiz tidak ditemukan');
+    }
+
+    if (body.update_type === 'update_quiz') {
+      return this.prisma.assessment.update({
+        where: {
+          ass_id: body.ass_id,
+        },
+        data: {
+          title: body.title,
+          description: body.description,
+          updated_by: body.by,
+        },
+        select: {
+          ass_id: true,
+        },
+      });
+    }
+
+    if (body.update_type == 'update_question') {
+      const promises = [];
+
+      promises.push(
+        this.prisma.assessmentQuestion.update({
+          where: {
+            assq_id: body.questions[0].assq_id,
+          },
+          data: {
+            text: body.questions[0].text,
+            explanation: body.questions[0].explanation,
+            updated_by: body.by,
+          },
+        }),
+      );
+
+      for (const option of body.questions[0].options) {
+        promises.push(
+          this.prisma.assessmentOption.updateMany({
+            where: {
+              assq_id: body.questions[0].assq_id,
+              asso_id: option.asso_id,
+            },
+            data: {
+              text: option.text,
+              is_correct: option.is_correct,
+              updated_by: body.by,
+            },
+          }),
+        );
+      }
+
+      await Promise.all(promises);
+
+      return body.questions[0];
+    }
+
+    if (body.update_type == 'add_question') {
+      const uid = new ShortUniqueId({ length: 12 });
+      const count = await this.prisma.assessmentQuestion.count({
+        where: { ass_id: body.ass_id },
+      });
+
+      const question = body.questions[0];
+
+      return this.prisma.assessmentQuestion.create({
+        data: {
+          assq_id: `ROQ${uid.rnd().toUpperCase()}`,
+          ass_id: body.ass_id,
+          type: question.type,
+          url: question.url,
+          explanation: question.explanation,
+          number: count + 1,
+          text: question.text,
+          created_by: body.by,
+          updated_by: body.by,
+          option: {
+            createMany: {
+              data: question.options.map((option) => {
+                return {
+                  asso_id: `ROO${uid.rnd().toUpperCase()}`,
+                  text: option.text,
+                  is_correct: option.is_correct,
+                  created_by: body.by,
+                  updated_by: body.by,
+                };
+              }),
+            },
+          },
+        },
+      });
+    }
+  }
+
+  async deleteQuestion(params: { ass_id: string; assq_id: string }) {
+    const quiz = await this.prisma.assessmentQuestion.count({
+      where: { ass_id: params.ass_id, assq_id: params.assq_id },
+    });
+
+    if (!quiz) {
+      throw new NotFoundException('Quiz atau question tidak ditemukan');
+    }
+
+    await this.prisma.assessmentQuestion.delete({
+      where: { ass_id: params.ass_id, assq_id: params.assq_id },
+    });
+
+    const questions = await this.prisma.assessmentQuestion.findMany({
+      where: { ass_id: params.ass_id },
+      select: {
+        assq_id: true,
+      },
+    });
+
+    const promises = [];
+
+    for (const [index, question] of questions.entries()) {
+      promises.push(
+        this.prisma.assessmentQuestion.update({
+          where: {
+            ass_id: params.ass_id,
+            assq_id: question.assq_id,
+          },
+          data: {
+            number: index + 1,
+          },
+        }),
+      );
+    }
+
+    await Promise.all(promises);
+
+    return params;
+  }
+}
