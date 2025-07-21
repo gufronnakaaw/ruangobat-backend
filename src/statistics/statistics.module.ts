73702ed2de9f9a6e449343eea87e6abc349e0992
gufronnakaaw <gufronnakaaw@gmail.com>
2025-07-21 16:03:32 +0700

    feat(statistics): add HttpModule

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -1,9 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../utils/services/prisma.service';
 import { StatisticsController } from './statistics.controller';
 import { StatisticsService } from './statistics.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [StatisticsController],
   providers: [StatisticsService, PrismaService],
 })
