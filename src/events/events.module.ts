446767019474d04df86f68c49240830b1b0657b1
gufronnakaaw <gufronnakaaw@gmail.com>
2025-10-01 13:58:33 +0700

    fix(events): add StorageService

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../utils/services/prisma.service';
+import { StorageService } from '../utils/services/storage.service';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService, PrismaService],
+  providers: [EventsService, PrismaService, StorageService],
 })
 export class EventsModule {}
